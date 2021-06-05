7d100e6669e02678ec06d5009e17665be708a844
AlexTaglia <alex.tagliabue@gmail.com>
2021-06-05 17:53:47 +0200

    Aggiunta versione con while

--- js-snack-12/js/script.js
+++ js-snack-12/js/script.js
@@ -12,11 +12,14 @@ var outputList = document.getElementById('list');
 var firstName = ['Topolino', 'Paperino', 'Minni', 'Paperina', 'Pippo', 'Pluto', 'Cip', 'Ciop', 'Paperone'];
 var lastName = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli', 'Rosa', 'Azzurri', 'Brown'];
 var guestQty = 3;
+var guestList =[];
 
 outputHtml.innerHTML =  'La lista degli invitati: ';
 
 var listHtml = '';
 
+// for -----------------------------------
+/*
 for (var i = 0; i < guestQty; i++) {
     var randomNumfirstName = Math.floor(Math.random() * firstName.length);
     var randomNumlastName = Math.floor(Math.random() * lastName.length);
@@ -27,5 +30,19 @@ for (var i = 0; i < guestQty; i++) {
 }
 
 outputList.innerHTML = listHtml;
+*/
 
+// while -----------------------------------
+while (guestList.length < guestQty){
+    var randomNumfirstName = Math.floor(Math.random() * firstName.length);
+    var randomNumlastName = Math.floor(Math.random() * lastName.length);
+    
+    var randomGuest = firstName[randomNumfirstName] + " " + lastName[randomNumlastName];
+    
+    if(!guestList.includes(randomGuest)){
+        guestList.push(randomGuest);
+        listHtml += '<li>' + randomGuest + '</li>';
+    }
+}
+outputList.innerHTML = listHtml;
 
